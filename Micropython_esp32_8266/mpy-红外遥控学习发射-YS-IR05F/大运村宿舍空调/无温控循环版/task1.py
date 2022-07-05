107b7f4ab85a167b95e85f4c15bfc18e6b7ba9c3
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-07-05 19:56:55 +0800

    up

--- Micropython_esp32_8266/mpy-红外遥控学习发射-YS-IR05F/大运村宿舍空调/无温控循环版/task1.py
+++ Micropython_esp32_8266/mpy-红外遥控学习发射-YS-IR05F/大运村宿舍空调/无温控循环版/task1.py
@@ -31,24 +31,27 @@ if 'flag' not in ret2:
 print(ret2['flag'])
 if 'cnt' not in ret2:
     ret2['cnt'] = 0
+print(ret2['cnt'])
 
 def task_main():
     global u,ret,led,ret2,c2
+    # cnt=0时开启 & flag=1
+    if ret2['cnt'] == 0:
+        u.write(ret['open'])
+        ret2['flag'] = 1
+        print("open")
+
     # cnt++ & status
     if ret2['cnt'] > 60 and ret2['flag']==1:
         # 关闭
         u.write(ret['open'])
         ret2['flag'] = 0
+        print("close")
     elif ret2['cnt'] > 150:
         # 重置
         ret2['cnt'] = 0
     else:
         ret2['cnt'] += 1
-    
-    # cnt=0时开启 & flag=1
-    if ret2['cnt'] == 0:
-        u.write(ret['open'])
-        ret2['flag'] = 1
     c2.writeConfig(ret2)
     return 0
 
