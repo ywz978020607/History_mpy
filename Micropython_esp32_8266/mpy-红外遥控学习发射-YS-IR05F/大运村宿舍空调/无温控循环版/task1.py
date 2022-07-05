f349d1d4dd0a0ade1be9cd22035a124e771ec4ce
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-07-05 19:28:33 +0800

    add air

---
+++ Micropython_esp32_8266/mpy-红外遥控学习发射-YS-IR05F/大运村宿舍空调/无温控循环版/task1.py
@@ -0,0 +1,49 @@
+import dht
+from machine import *
+from machine import UART
+import time
+import machine
+from config import *
+
+led = Pin(2,Pin.OUT)
+led.on()
+time.sleep(1)
+led.off()
+u=UART(2,9600)
+c=config('data.ini')
+ret = c.readAll()
+
+c2=config('status.ini')
+ret2 = c2.readAll()
+u.read()
+##################################
+if 'flag' not in ret2:
+    ret2['flag'] = 0
+print(ret2['flag'])
+if 'cnt' not in ret2:
+    ret2['cnt'] = 0
+
+def task_main():
+    global u,ret,led,ret2,c2
+    # cnt++ & status
+    if ret2['cnt'] > 60 and ret2['flag']==1:
+        # 关闭
+        u.write(ret['open'])
+        ret2['flag'] = 0
+    elif ret2['cnt'] > 150:
+        # 重置
+        ret2['cnt'] = 0
+    else:
+        ret2['cnt'] += 1
+    
+    # cnt=0时开启 & flag=1
+    if ret2['cnt'] == 0:
+        u.write(ret['open'])
+        ret2['flag'] = 1
+    c2.writeConfig(ret2)
+    return 0
+
+task_main()
+
+machine.deepsleep(60000) # 休眠ms
+# reset()
