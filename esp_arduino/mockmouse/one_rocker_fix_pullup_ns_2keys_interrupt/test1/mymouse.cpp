fa88c856aea8d03510a7027431b4fb23bd26fee8
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-07-30 21:31:10 +0800

    up

---
+++ esp_arduino/mockmouse/one_rocker_fix_pullup_ns_2keys_interrupt/test1/mymouse.cpp
@@ -0,0 +1,77 @@
+#include "mymouse.h"
+
+void Mymouse::set_up(){
+    // init adc
+    //set the resolution to 12 bits (0-4096)
+    analogReadResolution(adc_bit);
+    // set bias
+    Serial.println((get_bias(rocker_x) + get_bias(rocker_y))/2, DEC);
+    adc_bias_rocker = (get_bias(rocker_x) + get_bias(rocker_y))/2;
+    // init gpio when not interrupt
+    pinMode(s_key_1, INPUT_PULLUP); // INPUT
+    pinMode(rocker_key,INPUT_PULLDOWN); // 34-39 can only be input and can not PULL UP/DOWN.
+
+    // Interrupt
+    attachInterrupt(digitalPinToInterrupt(s_key_1), std::bind(&Mymouse::isr,this), FALLING);
+    // Serial.printf("mymouse init\n");
+    // init ble
+    bleMouse.begin();
+}
+
+void Mymouse::self_main(){
+    // KEY 中断类
+    // if(s_key_1.checkPressed()){
+    // press_key(MOUSE_LEFT);
+    // }
+    // KEY非中断
+//     if(digitalRead(s_key_1) == LOW){
+//         delay(150); // 消抖
+// //        Serial.println("push left button.\n");
+//         press_key(MOUSE_LEFT);
+//         //消抖
+//         while(digitalRead(s_key_1) == LOW);
+//         delay(150);
+//     }
+    if(digitalRead(rocker_key) == HIGH){ // 注意要硬件/软件上拉
+        // 摇杆键多功能： 模式切换&普通右键
+        delay(150); // 消抖
+        
+        delay(400);
+        if(digitalRead(rocker_key) == LOW){
+            mode = !mode;    
+//            Serial.println("change mode");
+//            Serial.println(mode?1:0, DEC);
+        }
+        else{
+            press_key(MOUSE_RIGHT);
+//            Serial.println("right");
+        }
+        while(digitalRead(rocker_key) == HIGH);
+        delay(150);
+    }
+
+    // 摇杆 or 滚轮
+    temp_val_1 = get_ADC(rocker_x, adc_bias_rocker);
+    temp_val_2 = get_ADC(rocker_y, adc_bias_rocker);
+//    Serial.println("temp_val:");
+//    Serial.println(temp_val_1, DEC);
+//    Serial.println(temp_val_2, DEC);
+    if(mode){
+        // 滚轮
+        temp_val_1 = change_speed(temp_val_1, true);
+        temp_val_2 = change_speed(temp_val_2);
+        if(temp_val_1 != 0 || temp_val_2 != 0){
+        //  move_scroll_right(temp_val_1);
+        //  move_scroll_up(temp_val_2);
+            move_scroll_up_right(temp_val_1, temp_val_2);
+        }
+    }
+    else{
+        // 指针
+        temp_val_1 = change_speed(temp_val_1);
+        temp_val_2 = change_speed(temp_val_2, true);
+        if(temp_val_1 != 0 || temp_val_2 != 0){
+            move_point_right_down(temp_val_2, temp_val_1);
+        }
+    }
+}
---
+++ esp_arduino/mockmouse/one_rocker_fix_pullup_ns_2keys_interrupt/test1/mymouse.h
@@ -0,0 +1,107 @@
+
+
+#ifndef _MYMOUSE_H_
+#define _MYMOUSE_H_
+ 
+#include <Arduino.h>
+#include <FunctionalInterrupt.h>
+#include <BleConnectionStatus.h>
+#include <BleMouse.h>
+// #include "Button.h"
+
+class Mymouse {
+  public:
+    // Mymouse();
+    // ~Mymouse();
+    bool isConnected(){
+        return bleMouse.isConnected();
+    }
+    void set_up();
+    void self_main(); // 主函数
+    // 
+    int get_bias(int pin_val){
+      int avg_val = 0;
+      int avg_num = 5;
+      for(int ii=0;ii<avg_num;ii++){
+        avg_val += analogRead(pin_val);
+        delay(50);
+      }
+      avg_val = avg_val/avg_num;
+      return ((1<<(adc_bit - 1)) - avg_val);
+    }
+    int get_ADC(int pin_val, int temp_add_bias = 0){
+      return min(analogRead(pin_val) + temp_add_bias, 1<< adc_bit);
+    }
+    int change_speed(unsigned int val, bool oppo = false){
+      val += ( 1 << (adc_bit - rank_num - 1) ); //输出挡位区间的一半 [0, 2*rank_num] -> [-xx,xx]
+      val = val >> (adc_bit - rank_num + 1);  // 缩放到只关注2*rank_num挡位
+      return oppo?(rank_num - val):(val - rank_num);
+    }
+    void move_point_right_down(int val1, int val2){
+        if(val1 == 0 && val2 == 0){
+          return;
+        }
+        for(unsigned char ii=0;ii<10;ii++){
+          bleMouse.move(val1*2, val2*2); //数值可改变速度
+          delay(10); // 修改delay同样可以改变速度 - 作为效果调整使用
+        }
+      }
+
+    void move_scroll_up_right(int val1, int val2){
+        if(val1 == 0 && val2 == 0){
+            return;
+          }
+        for(unsigned char ii=0;ii<2;ii++){
+          bleMouse.move(0,0, val1, val2); //数值可改变速度
+          delay(100); // 修改delay同样可以改变速度 - 作为效果调整使用
+        }
+      }
+
+    //
+    void press_key(uint8_t mode){
+      // 支持 MOUSE_LEFT MOUSE_RIGHT MOUSE_MIDDLE MOUSE_BACK MOUSE_FORWARD 以及 MOUSE_LEFT | MOUSE_RIGHT 可多元复合操作
+      bleMouse.click(mode);
+      delay(500);
+    }
+
+  public:
+    // 轮询代替中断 34-39只能输入且不能上下拉
+    int rocker_key = 33; //摇杆自带
+    int rocker_x = 34;
+    int rocker_y = 35;
+
+    const int s_key_1 = 26;
+
+    BleMouse bleMouse;
+
+    int temp_val_1 = 0;
+    int temp_val_2 = 0;
+    int adc_bit = 12;
+    int rank_num = 4; //4挡 [0,8] 9个值 -- 由于默认向下取整，故加上2^(adc_bit-rank_num-1); // 2^(adc_bit - rank_num)为1 故加上一半的bias用来取整
+    int adc_bias_rocker = 144; // 补偿-调试时中间位置与中间值的采样差 -- 摇杆模块采用3.3V供电  自动校正
+    bool mode = false; // true: scroll
+
+
+    // interrupt
+    void ARDUINO_ISR_ATTR isr(){
+      detachInterrupt(digitalPinToInterrupt(s_key_1));
+      // noInterrupts();
+      // if (millis() - triggertime > 50 && digitalRead(s_key_1) == LOW)//如果中断内时间减去上次进入中断时间大于消抖时间
+      // {
+      //   triggertime =millis();
+      //   Serial.printf("pressed\n");
+      // }
+      delay(50);
+      if (digitalRead(s_key_1) == LOW)
+      {
+        triggertime =millis();
+      }
+
+      attachInterrupt(digitalPinToInterrupt(s_key_1), std::bind(&Mymouse::isr,this), FALLING);
+      // interrupts();
+    }
+    volatile uint32_t triggertime;
+
+};
+
+#endif
